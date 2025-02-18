f3854efe9d4e7382f385369b4b0296b44366dd20
Tarun sharma <80513443+tarunaddictivemedia@users.noreply.github.com>
2025-02-18 16:21:11 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$('.testimonialslider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    fade: false,
+    dots: false,
+    arrows: true,
+});
+
+$('.sucessstorieslider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    variableWidth: true,
+    dots: false,
+    arrows: true,
+});
