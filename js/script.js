949e976f389d15056e1fb557f50bbd479e4ac0b3
Tarun sharma <80513443+tarunaddictivemedia@users.noreply.github.com>
2025-02-21 01:14:14 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ $('.testimonialslider').slick({
     fade: false,
     dots: false,
     arrows: true,
+    responsive: [
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
 });
 
 $('.sucessstorieslider').slick({
@@ -13,4 +25,35 @@ $('.sucessstorieslider').slick({
     variableWidth: true,
     dots: false,
     arrows: true,
+});
+$(".humburgur").click(function() {
+    $('.menuwrapper').show();
+});
+$(".menuwrapper .close").click(function() {
+    $('.menuwrapper').hide();
+});
+$("#aboutsection").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".aboutsection").offset().top},
+        'slow');
+});
+$("#featuresection").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".featuresection").offset().top},
+        'slow');
+});
+$("#sucessstoriesection").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".sucessstoriesection").offset().top},
+        'slow');
+});
+$("#Testimonialssection").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".ourtestimonialsec").offset().top},
+        'slow');
+});
+$("#getintouch").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".ourofficesec").offset().top},
+        'slow');
 });
